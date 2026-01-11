ecb7e274ba7ea3954e06233543c310991de60e06
Anton Utz <alien-coding@outlook.com>
2026-01-11 16:34:11 +0100

    using plane xml now as comp

--- scripts/train.py
+++ scripts/train.py
@@ -32,7 +32,7 @@ PPO_CONFIG = {
 }
 
 ENV_CONFIG = {
-    "xml_file": "humanoid.xml",  # NOTE: full name of a file in ./models. If empty / none, defaults are used
+    "xml_file": "humanoid_plane.xml",  # NOTE: full name of a file in ./models. If empty / none, defaults are used
     "env_id": "HumanoidEnvCurr",  # "HumanoidEnvDefault", "HumanoidEnvBase", "HumanoidEnvCurr"
     "n_envs": 8,
     "max_steps": 0,  # 0 disables max steps
