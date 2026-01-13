d5cff3b2379fd17c7b2358a3923b82ba3b1f28bf
Anton Utz <alien-coding@outlook.com>
2026-01-13 12:10:35 +0100

    increased contact cost weight further

--- scripts/train.py
+++ scripts/train.py
@@ -49,7 +49,7 @@ ENV_CONFIG = {
     "norm_obs": True,
     "clip_reward": 10,  # default: 10
     "env_kwargs": {
-        "contact_cost_weight": 5e-6,  # default 5e-7
+        "contact_cost_weight": 5e-5,  # default 5e-7
     },
 }
 
