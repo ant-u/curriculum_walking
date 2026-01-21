e7d5b4190a7c065378c4f74cd4913919bc5a9230
Anton Utz <Ant.Utz@campus.lmu.de>
2026-01-21 11:09:12 +0100

    finished training

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=bipedal-walking-training-20M
+#SBATCH --job-name=bipedal-walking-training-15M
 #SBATCH --comment="Training for robotic RL bipedal locomotion."
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=Ant.Utz@campus.lmu.de
 #SBATCH -D ./
-#SBATCH -o ./runs/humanoidenvcurr_ppo_lr1e-04_seed0_20260110-002402/logs/slurm.%j.%N.out
-#SBATCH -e ./runs/humanoidenvcurr_ppo_lr1e-04_seed0_20260110-002402/logs/slurm.%x.%j.err
-#SBATCH --partition=NvidiaAll
+#SBATCH -o ./runs/humanoidenvcurr_ppo_lr1e-04_seed0_20260116-190322/logs/slurm.%j.%N.out
+#SBATCH -e ./runs/humanoidenvcurr_ppo_lr1e-04_seed0_20260116-190322/logs/slurm.%x.%j.err
+#SBATCH --partition=Krater
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 
 source ./.venv/bin/activate
-python -u -m scripts.train -p runs/humanoidenvcurr_ppo_lr1e-04_seed0_20260110-002402 -m "20M training with default xml file to find out what more training time does"
+python -u -m scripts.train -p runs/humanoidenvcurr_ppo_lr1e-04_seed0_20260116-190322 -m "moved geoms to worldbody, creating with contype=conaffinity=1, disabling with script. fast training?"
