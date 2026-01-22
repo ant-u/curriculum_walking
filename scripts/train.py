6c1e11eb0c1222e85781ce94989d3d08089753e7
Anton Utz <alien-coding@outlook.com>
2026-01-22 11:41:45 +0100

    changing for 30m lidar training

--- scripts/train.py
+++ scripts/train.py
@@ -26,7 +26,7 @@ PPO_CONFIG = {
     "normalize_advantage": True,
     "verbose": 1,
     "tensorboard_log": True,
-    "timesteps": 15e6,
+    "timesteps": 30e6,
     "seed": 0,
     "partition": "Krater",  # e.g. NvidiaAll or Krater
 }
@@ -36,8 +36,8 @@ ENV_CONFIG = {
     "env_id": "HumanoidEnvCurr",  # "HumanoidEnvDefault", "HumanoidEnvBase", "HumanoidEnvCurr"
     "n_envs": 8,
     "max_steps": 0,  # 0 disables max steps
-    "use_lidar": False,
-    "render_lidar": False,  # NOTE: is disabled during training
+    "use_lidar": True,
+    "render_lidar": True,  # NOTE: is disabled during training
     "use_relative_height": False,
     "seed": 0,
     "n_points_x": 6,
