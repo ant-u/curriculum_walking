15867a75ed91c9396c2033cab05fbb30a1606451
Anton Utz <alien-coding@outlook.com>
2026-02-23 15:29:19 +0100

    changed min foot torso distance

--- envs/config.py
+++ envs/config.py
@@ -33,7 +33,7 @@ ENV_CONFIG = {
     "use_levels": True,
     "geom_z_gap": 1e-3,  # default 1e-3 to not match 0 plane and ghost geoms
     "terminate_at_x_border": 60,  # 0 disables it, midd of end platform at 60
-    "min_diff_torso_feet": 0.6,  # reset if height diff of feet (averaged) and torso lower than this
+    "min_diff_torso_feet": 0.4,  # reset if height diff of feet (averaged) and torso lower than this
     "healthy_z_range": [-10,10],  # range in which torso is allowed to be
     "exclude_absolute_height": True,  # exclude the abs height from observation
     "use_relative_height": False,  # used for lidar, if false, lidar observation is absolute height
