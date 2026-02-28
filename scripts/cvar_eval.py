db13e0d580e1b0bb7035a904031b5279f2edb4d2
Anton Utz <alien-coding@outlook.com>
2026-02-28 16:27:53 +0100

    adaption

--- scripts/cvar_eval.py
+++ scripts/cvar_eval.py
@@ -195,7 +195,7 @@ MAX_INIT = [1,1,1,1,1]
 #                             1000, [0.1, 0.5, 1, 2, 5, 10, 25, 50, 75, 100], 10)
 
 cvar_with_predefined_levels("runs/result_exp_d/humanoidenvcurr_ppo_lr5e-05_seed0_20260223-154623",
-                            "checkpoints/easy/best_model.zip", "easy/best_vecnormalize_stats.pkl",
+                            "checkpoints/easy/best_model.zip", "checkpoints/easy/best_vecnormalize_stats.pkl",
                             "runs/result_exp_d/humanoidenvcurr_ppo_lr5e-05_seed0_20260223-154623/eval",
                             "runs/base_lidar_gait_height_resistant/eval/cvar_buffer_dump.pkl",
                             1000, [0.1, 0.5, 1, 2, 5, 10, 25, 50, 75, 100], 10)
