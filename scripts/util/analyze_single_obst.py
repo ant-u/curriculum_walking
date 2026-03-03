04e0178fe8cab515bb19e182a407cbfd3819023f
Anton Utz <alien-coding@outlook.com>
2026-03-03 22:21:30 +0100

    new plots

--- scripts/util/analyze_single_obst.py
+++ scripts/util/analyze_single_obst.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+from matplotlib import pyplot as plt
 import numpy as np
 from stable_baselines3 import PPO
 from envs.config import CALLBACK_CONFIG
@@ -82,5 +83,53 @@ def view_vec_env(run_dir: str, save_path, display_steps: int = 2500) -> list:
         pickle.dump({"runs": runs, "diff": difficulties, "types": ["Slab", "Stairs", "Stump", "Gap", "neg Slab", "neg Stairs"]}, f)
 
 
+
+def create_plot(data_path, save_path, position="lower left"):
+    with open(data_path, "rb") as f:
+        data = pickle.load(f)
+
+    x = data["diff"]*100
+    y = [[],[],[],[],[],[]]
+    for i, run in enumerate(data["runs"]):
+        y[i] = [100*np.mean(x) for x in run]
+
+    font_size_axis_ticks = 12
+    font_size_title = 12
+    font_size_label = 12
+    font_size_legend = 12
+
+    fig, ax1 = plt.subplots(figsize=(8, 4))
+
+    ax1.set_xlabel("Obstacle difficulty (in %)", fontsize=font_size_label)
+    ax1.set_ylabel("Success rate (in %)", fontsize=font_size_label)  # color=color_reward
+
+    colors = [(0.5, 1, 0.5), (1, 0.5, 0.5), (0.5, 0.5, 1), (0.5, 1, 1)]
+
+    line1, = ax1.plot(x, y[0], color=colors[0], linewidth=2, label="Slab")
+    line2, = ax1.plot(x, y[4], color=colors[0], linewidth=2, linestyle="--", label="Slab (desc.)")  #linestyle="--"
+    line3, = ax1.plot(x, y[1], color=colors[1], linewidth=2, label="Stairs")  
+    line4, = ax1.plot(x, y[5], color=colors[1], linewidth=2, linestyle="--", label="Stairs (desc.)")  #linestyle="--"
+    line5, = ax1.plot(x, y[2], color=colors[2], linewidth=2, label="Stump")  
+    line6, = ax1.plot(x, y[3], color=colors[3], linewidth=2, label="Gap")
+
+    ax1.tick_params(axis="y", labelsize=font_size_axis_ticks) #labelcolor=color_reward
+    ax1.tick_params(axis="x", labelsize=font_size_axis_ticks) #labelcolor=color_reward
+    ax1.set_ylim(bottom=0, top=100)
+    ax1.set_xlim(left=0, right=100)
+    ax1.set_xticks(ticks=x[::2], labels=[int(x) for x in x[::2]])
+
+
+    lines = [line1, line2, line3, line4, line5, line6]
+    labels = [l.get_label() for l in lines]
+    ax1.legend(lines, labels, loc=position, fontsize=font_size_legend)
+    # plt.title(f'Buffer Evolution', fontsize=font_size_title) #, fontweight="bold")
+
+    fig.tight_layout()
+    plt.savefig(save_path)
+    plt.show()
+    # plt.savefig("thesis/plots/training_progress_base_policy.svg")
+
 # view_vec_env("runs/result_exp_f/humanoidenvcurr_ppo_lr1e-04_seed0_20260224-120350", "runs/result_exp_f/humanoidenvcurr_ppo_lr1e-04_seed0_20260224-120350/eval/single_obstacle_results.pkl")
-view_vec_env("runs/base_lidar_gait_height_resistant", "runs/base_lidar_gait_height_resistant/eval/single_obstacle_results_z_range.pkl")
+# view_vec_env("runs/base_lidar_gait_height_resistant", "runs/base_lidar_gait_height_resistant/eval/single_obstacle_results_z_range.pkl")
+create_plot("runs/result_exp_f/humanoidenvcurr_ppo_lr1e-04_seed0_20260224-120350/eval/single_obstacle_results.pkl", "thesis_plots/experiments/plots/exp_f_single_obst.pdf")
+create_plot("runs/base_lidar_gait_height_resistant/eval/single_obstacle_results_z_range.pkl", "thesis_plots/base_policy/base_policy_single_obst.pdf", "upper right")
